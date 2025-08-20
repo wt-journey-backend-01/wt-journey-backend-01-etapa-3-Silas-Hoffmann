95556930430498f4454f3c4df2b49352390bec0a
Silas <silasrviana@gmail.com>
2025-08-19 21:47:07 -0300

    knexfile padrao

--- knexfile.js
+++ knexfile.js
@@ -4,40 +4,40 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-const config = {
+
+module.exports = {
+
   development: {
-    client: "pg",
+    client: 'pg',
     connection: {
-      host: process.env.DB_HOST || "127.0.0.1",
-      port: process.env.DB_PORT || 5432,
+      host: '127.0.0.1',
+      port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
     },
     migrations: {
-      directory: path.resolve(__dirname, "db/migrations"),
+      directory: './db/migrations',
     },
     seeds: {
-      directory: path.resolve(__dirname, "db/seeds"),
+      directory: './db/seeds',
     },
   },
-
   ci: {
-    client: "pg",
+    client: 'pg',
     connection: {
-      host: process.env.DB_HOST || "postgres",
-      port: process.env.DB_PORT || 5432,
+      host: 'postgres',
+      port: 5432,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
     },
     migrations: {
-      directory: "./db/migrations",
+      directory: './db/migrations',
     },
     seeds: {
-      directory: "./db/seeds",
+      directory: './db/seeds',
     },
-  },
-};
+  }
 
-module.exports = config[process.env.NODE_ENV || "development"];
+};
