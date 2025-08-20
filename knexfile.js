7d66e7fc9fb1b281f8b22a0a1d3681c14ee6a37f
Silas <silasrviana@gmail.com>
2025-08-19 21:21:44 -0300

    alteração do knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,9 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
 module.exports = {
   development: {
     client: "pg",
@@ -16,6 +19,22 @@ module.exports = {
     },
     seeds: {
       directory: path.resolve(__dirname, "db/seeds"),
+    },
+  },
+  ci: {
+    client: "pg",
+    connection: {
+      host: process.env.DB_HOST || "postgres",
+      port: process.env.DB_PORT || 5432,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
     },
   },
 };
